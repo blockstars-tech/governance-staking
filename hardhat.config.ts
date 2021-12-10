d9dfc5fd5f8506aff80b15bded4cd99c899363d1
Alexandr Martirosyan <alexandr.martirosyan2000@gmail.com>
2021-12-10 16:45:24 +0400

    initial version of contracts

---
+++ hardhat.config.ts
@@ -0,0 +1,41 @@
+import "dotenv/config";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-truffle5";
+import "@nomiclabs/hardhat-web3";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  typechain: {
+    target: "truffle-v5",
+    alwaysGenerateOverloads: true,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
